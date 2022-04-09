2949773767225e71ea594248b594123d42e03de5
Dradarus <101935503+Dradarus@users.noreply.github.com>
2022-04-09 17:14:01 +0300

    Add files via upload

---
+++ final_win.py
@@ -0,0 +1,88 @@
+from PyQt5 import Qt
+from PyQt5.QtWidgets import (
+    QApplication, QLabel, QPushButton, QVBoxLayout, QHBoxLayout, QLineEdit, QWidget, QGroupBox, QRadioButton,QListWidget)
+from instr import *
+class FinalWin(QWidget):
+    def __init__(self, exp, set_appear, initUI,show):
+        super().__init__()
+        self.set_appear()
+        self.initUI()
+        self.show()
+        self.exp = exp()
+    def set_appear(self):
+        self.setWindowTitle(txt_title)
+        self.resize(win_width, win_weight)
+        self.move(win_x, win_y)
+    def initUI(self):
+        self.workh_text = QLabel(txt_workbench + self.results())
+        self.index_text = QLabel(txt_index +  str(self.index))
+        self.layout_line = QVBoxLayout()
+        self.layout_line.addWidget(self.index_text, alignment = Qt.AlignLeft)
+        self.layout_line.addWidget(self.workh_text, alignment = Qt.AlignLeft)
+        self.setLayout(self.layout_line)
+    def results(self):
+        if self.exp.age < 7:
+            self.index = 0
+            return 'нет данных для такого возраста'
+        self.index= (4*(int(self.exp.t1)+ int(self.exp.t2)+ int(self.exp.t3))-200)/10
+        if self.exp.age == 7 or self.exp.age == 8:
+            if self.index >= 21:
+                return txt_res1
+            if self.index < 21 and self.index >= 17:
+                return txt_res2
+            if self.index < 17 and self.index >= 12:
+                return txt_res3
+            if self.index < 12 and self.index >= 6.5:
+                return txt_res4
+            else:
+                return txt_res5
+        if self.exp.age == 9 or self.exp.age == 10:
+            if self.index >= 19.5:
+                return txt_res1
+            if self.index < 19.5 and self.index >= 15.5:
+                return txt_res2
+            if self.index < 15.5 and self.index >= 12=0.5:
+                return txt_res3
+            if self.index < 10.5 and self.index >= 5:
+                return txt_res4
+            else:
+                return txt_res5
+        if self.exp.age == 11 or self.exp.age == 12:
+            if self.index >= 18:
+                return txt_res1
+            if self.index < 18 and self.index >= 14:
+                return txt_res2
+            if self.index < 14 and self.index >= 9:
+                return txt_res3
+            if self.index < 9 and self.index >= 3.5:
+                return txt_res4
+            else:
+                return txt_res5
+       if self.exp.age == 13 or self.exp.age == 14:
+            if self.index >= 16.5:
+                return txt_res1
+            if self.index < 16.5 and self.index >= 12.5:
+                return txt_res2
+            if self.index < 12.5 and self.index >= 7.5:
+                return txt_res3
+            if self.index < 7.5 and self.index >= 2:
+                return txt_res4
+            else:
+                return txt_res5
+        if self.exp.age >= 15:
+            if self.index >= 15:
+                return txt_res1
+            if self.index < 15 and self.index >= 11:
+                return txt_res2
+            if self.index < 11 and self.index >= 6:
+                return txt_res3
+            if self.index < 6 and self.index >= 0.5:
+                return txt_res4
+            else:
+                return txt_res5
+        
+            
+            
+app = QApplication([])
+a = FinalWin()
+app.exec_()
